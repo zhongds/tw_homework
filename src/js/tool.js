bd68fd148518f384f18f7adc1429501f103978cb
zhongds <1326581318@qq.com>
2017-02-28 08:55:22 +0800

    Update tool.js

--- src/js/tool.js
+++ src/js/tool.js
@@ -3,13 +3,15 @@ const tool = {}
 tool.siblings = (target) => {
     const a = []
     const p = target.parentNode.children
-    for (let el of p) {
+    for (let i=0; i<p.length; i++) {
+        let el = p[i]
         if (el !== target) a.push(el)
     }
     return a
 }
 tool.removeActiveClass = (arr) => {
-    for (let el of arr) {
+    for (let i=0; i<arr.length; i++) {
+        let el = arr[i]
         el.classList.remove('active')
     }
 }
